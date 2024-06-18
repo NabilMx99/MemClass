0de493d62c13c529cb7fa6da84a28bd4265cf31b
Nabil Al Masri <142125053+NabilMx99@users.noreply.github.com>
2024-06-18 17:10:55 +0300

    Update Memory.cpp

--- MemClass/Memory.cpp
+++ MemClass/Memory.cpp
@@ -3,105 +3,95 @@
 #include <TlHelp32.h>
 #include <cstdint>
 
+#pragma once
+
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <cstdint>
+
 class Memory
 
 {
 
 private:
 
-	DWORD processID;
-	HANDLE processHandle;
+	DWORD dwProcessId;
+	HANDLE hProc;
 	LPCSTR windowName;
 
 public:
 
 	Memory(LPCSTR windowName)
-
 	{
-
 		this->windowName = windowName;
-		GetWindowThreadProcessId(FindWindowA(NULL, this->windowName), &this->processID);
-		this->processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, this->processID);
-
+		HWND hWindow = FindWindowA(NULL, windowName);
+		GetWindowThreadProcessId(hWindow, &this->dwProcessId);
+		this->hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, this->dwProcessId);
 	}
 
-	template <class T>
+	template <typename T>
 
 	T ReadMemory(uintptr_t addressToRead)
-
 	{
-
 		T value;
-		ReadProcessMemory(this->processHandle, (LPCVOID)addressToRead, &value, sizeof(value), NULL);
+		ReadProcessMemory(this->hProc, (LPCVOID)addressToRead, &value, sizeof(value), NULL);
 		return value;
-
 	}
 
-	template <class T>
+	template <typename T>
 
 	void WriteMemory(uintptr_t addressToWrite, T desiredValue)
-
 	{
-
-		WriteProcessMemory(this->processHandle, (LPVOID)addressToWrite, &desiredValue, sizeof(desiredValue), NULL);
+		WriteProcessMemory(this->hProc, (LPVOID)addressToWrite, &desiredValue, sizeof(desiredValue), NULL);
 		return;
-
 	}
 
-	DWORD GetProcessID(LPCWSTR processName)
-
+	DWORD GetProcessId(LPCWSTR processName)
 	{
-		this->processID = 0;
-		HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+		this->dwProcessId = 0;
+		HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, dwProcessId);
 
 		if (hSnap != INVALID_HANDLE_VALUE)
-
 		{
 
 			PROCESSENTRY32W entry;
 			entry.dwSize = sizeof(entry);
 
 			if (Process32FirstW(hSnap, &entry))
-
 			{
 
 				do
-
 				{
-					if (!_wcsicmp(entry.szExeFile, processName))
 
+					if (!_wcsicmp(entry.szExeFile, processName))
 					{
 
-						processID = entry.th32ProcessID;
+						dwProcessId = entry.th32ProcessID;
 						break;
 
 					}
 
-				} while(Process32NextW(hSnap, &entry));
+				} while (Process32NextW(hSnap, &entry));
 			}
 		}
 
 		CloseHandle(hSnap);
-		return processID;
+		return dwProcessId;
 
 	}
 
 	uintptr_t GetModuleBase(LPCWSTR moduleName)
-
 	{
-
 		uintptr_t moduleBase = 0;
-		HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, this->processID);
+		HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, dwProcessId); 
 
 		if (hSnap != INVALID_HANDLE_VALUE)
-
 		{
 
 			MODULEENTRY32W entry;
 			entry.dwSize = sizeof(entry);
 
 			if (Module32FirstW(hSnap, &entry))
-
 			{
 
 				do
@@ -109,7 +99,6 @@ public:
 				{
 
 					if (!_wcsicmp(entry.szModule, moduleName))
-
 					{
 
 						moduleBase = (uintptr_t)entry.modBaseAddr;
